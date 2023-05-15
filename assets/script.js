56f87c0d516e2fb722fb5d819c471c4c23574e3b
Haseebz1 <haseebkandore@hotmail.com>
2023-05-15 23:08:43 +0100

    js functiion added for weather api

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,34 @@
-var cityInput = document.querySelector('#cityInput')
-var searchBtn = document.querySelector('#searchButton')
-var apiKey = 'cd2188794e9c4a18688851ec2409ea64'
-var baseUrl = 'https://api.openweathermap.org/data/2.5/'
+var searchBtn = document.querySelector("#searchButton");
+var apiKey = "cd2188794e9c4a18688851ec2409ea64";
+// var mainUrl =
+//   "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}";
+// var longLatUrl =
+//   "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
 
-async function fetchCoords(search){
-    const response = await fetch(`${baseUrl}weather?q=${search}&appid=${apiKey}`)
-    const data = await response.json()
-    console.log(data)
+// var baseUrl = "https://api.openweathermap.org/data/2.5/";
+
+async function fetchWeather() {
+  var search = document.querySelector("#cityInput").value;
+  const responseLatLon = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=1&appid=${apiKey}`
+  );
+
+  //const response = await fetch(`${baseUrl}weather?q=${search}&appid=${apiKey}`);
+  const dataLatLon = await responseLatLon.json();
+  var lat = dataLatLon[0].lat;
+  var longitude = dataLatLon[0].lon;
+
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${longitude}&appid=${apiKey}`
+  );
+  const data = await response.json();
+
+  document.querySelector("#weatherCurrentCityName").textContent =
+    data.city.name + " " + new Date(data.list[0].dt_txt).toLocaleDateString();
+
+  console.log(data);
 }
 
-searchBtn.addEventListener('click', fetchCoords)
+searchBtn.addEventListener("click", fetchWeather);
 
-var testCity = 'Birmingham'
-
-fetchCoords('London')
+//fetchCoords();
