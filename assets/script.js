500ad834693f934f7f8697007dfb490cdee9fa44
Haseebz1 <haseebkandore@hotmail.com>
2023-05-18 21:56:27 +0100

    starting new function removed old

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,29 @@
 var searchBtn = document.querySelector("#searchButton");
 var apiKey = "cd2188794e9c4a18688851ec2409ea64";
-// var mainUrl =
-//   "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}";
-// var longLatUrl =
-//   "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
 
-// var baseUrl = "https://api.openweathermap.org/data/2.5/";
+
+
 
 async function fetchWeather() {
   var search = document.querySelector("#cityInput").value;
-  const responseLatLon = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=1&appid=${apiKey}`
-  );
+  if (search === "") {
+    search = "London";
+    var localHistory = localStorage.getItem("localHistory");
+    if (localHistory === "" || localHistory === null) {
+      localStorage.setItem("localHistory", JSON.stringify([]))
+      var localHistoryList = JSON.parse(localStorage.getItem("localHistory"));
+      localHistoryList.push(search);
+      if(localHistoryList.length > 7)
+      localHistoryList.shift();
+      localStorage.setItem("localHistory", JSON.stringify(localHistoryList));
+    }
+    var localHistoryList = JSON.parse(localStorage.getItem("localHistory"));
+    var buttonContainer = document.querySelector("#buttonContainer");
+    while (buttonContainer.firstChild) {
+      buttonContainer.removeChild(buttonContainer.firstChild);
+    }
+    
 
-  //const response = await fetch(`${baseUrl}weather?q=${search}&appid=${apiKey}`);
-  const dataLatLon = await responseLatLon.json();
-  var lat = dataLatLon[0].lat;
-  var longitude = dataLatLon[0].lon;
-
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${longitude}&appid=${apiKey}`
-  );
-  const data = await response.json();
-
-  document.querySelector("#weatherCurrentCityName").textContent =
-    data.city.name + " " + new Date(data.list[0].dt_txt).toLocaleDateString();
-
-  console.log(data);
-}
 
 searchBtn.addEventListener("click", fetchWeather);
 
